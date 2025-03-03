6ac22eadb25f1ba1effc1c2e74d2fa5c93a5dc5b
MrPineappleCB <c24405434@myTUDublin.ie>
2025-03-03 14:30:10 +0000

    sure

--- the-course/CharacterBody2D.gd
+++ the-course/CharacterBody2D.gd
@@ -4,14 +4,14 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var score = 0
+var maxJump = 1
+var jumpCount = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	var maxJump = 2
-	var jumpCount = 0
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -20,8 +20,10 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or jumpCount<maxJump):
 		velocity.y = JUMP_VELOCITY
 		jumpCount += 1
+		#print(jumpCount)
 	if is_on_floor():
 		jumpCount = 0
+		#print(jumpCount)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -32,3 +34,13 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "jumpperson":
+	#self.queue_free()
+		respawn()
+	pass # Replace with function body.
+	
+func respawn():
+	position = Vector2(137,130)
