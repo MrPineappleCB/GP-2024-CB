7dff2dae09cecfc053dd52b961bf8a6bd7a76abf
MrPineappleCB <c24405434@myTUDublin.ie>
2025-03-13 14:38:43 +0000

    3D scene

--- the-course/fps_fella.gd
+++ the-course/fps_fella.gd
@@ -1,28 +1,50 @@
-extends CharacterBody3D
+extends Node3D
 
+@export var score = 0
+@export var speed:float = 10
+@export var rot_speed =0.5
+var controlling = true
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+var relative:Vector2 = Vector2.ZERO
 
+func _input(event):
+	if event is InputEventMouseMotion and controlling:
+		relative = event.relative
+	if event.is_action_pressed("ui_cancel"):
+		if controlling:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:            
+			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		controlling = ! controlling
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pass # Replace with function body.
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+@export var can_move:bool = true
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	rotate(Vector3.DOWN, deg_to_rad(relative.x * deg_to_rad(rot_speed) * delta))
+	rotate(transform.basis.x,deg_to_rad(- relative.y * deg_to_rad(rot_speed) * delta))
+	relative = Vector2.ZERO
+	if can_move:
+		var v = Vector3.ZERO
+		
+		var mult = 1
+		if Input.is_key_pressed(KEY_SHIFT):
+			mult = 3
+		
+		var turn = Input.get_axis("left", "right") - v.x    
+		if abs(turn) > 0:   
+			position = position + global_transform.basis.x * speed * turn * mult * delta
+			# global_translate(global_transform.basis.x * speed * turn * mult * delta)
+		
+		var movef = Input.get_axis("forward", "back")
+		if abs(movef) > 0:     
+			global_translate(global_transform.basis.z * speed * movef * mult * delta)
+		
+		var upanddown = Input.get_axis("ui_accept", "ui_cut")
+		if abs(upanddown) > 0:     
+			global_translate(- global_transform.basis.y * speed * upanddown * mult * delta)
